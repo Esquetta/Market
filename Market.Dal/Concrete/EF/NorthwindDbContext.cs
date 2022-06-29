ebc45b083b7cd809386a982d299be4a7fef30709
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-29 13:29:35 +0300

    DbContext Added.

---
+++ Market.Dal/Concrete/EF/NorthwindDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Market.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Dal.Concrete.EF
+{
+    public class NorthwindDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-EMPIGDT;Initial Catalog=NORTHWND;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
