88d8444304d638dbb3159870cc80bad4a833d44c
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-29 13:35:31 +0300

    RepositoryBase crud methods added.

--- Market.Dal/Abstract/IRepositoryBase.cs
+++ Market.Dal/Abstract/IRepositoryBase.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Market.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Market.Dal.Abstract
 {
-    public interface IRepositoryBase
+    public interface IRepositoryBase<Tentity> where Tentity : class, IEntity, new()
     {
+        void Add(Tentity tentity);
+        void Update(Tentity tentity);
+        void Delete(Tentity tentity);
+        Tentity Get(Expression<Func<bool, Tentity>> filter = null);
+        List<Tentity> GetAll(Expression<Func<bool, Tentity>> filter = null);
+        IQueryable<Tentity> GetAllInclude(Expression<Func<bool, Tentity>> filter = null, params Expression<Func<Tentity, object>>[] include);
+
     }
 }
