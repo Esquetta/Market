4b91f8d26690dcf7e7f9492ab12aa02e0f038160
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-29 13:38:00 +0300

    ICategoryDal added to EF directory.

---
+++ Market.Dal/Abstract/EF/ICategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using Market.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Dal.Abstract.EF
+{
+    public interface ICategoryDal:IRepositoryBase<Category>
+    {
+    }
+}
