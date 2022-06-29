163b83aba114bc292e6c7cc8319fd2a384489977
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-29 14:03:44 +0300

    Category and product dal added.

---
+++ Market.Dal/Concrete/EF/EFProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Market.Dal.Abstract.EF;
+using Market.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Dal.Concrete.EF
+{
+    public class EFProductDal:RepositoryBase<Product,NorthwindDbContext>,IProductDal
+    {
+    }
+}
