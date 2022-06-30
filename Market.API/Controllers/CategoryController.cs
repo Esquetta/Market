fd40eb85ff9958fc4ed61fffd2bd52e936e70da3
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-30 12:53:08 +0300

    Category Controller Added

---
+++ Market.API/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Market.BL.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categoires = categoryService.GetAll();
+            return Ok(categoires);
+        }
+    }
+}
